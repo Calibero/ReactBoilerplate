4f1c819c5df125deda87518a84bee593c761275c
Pavel Boček <paja.bocek@gmail.com>
2016-12-22 08:32:36 +0100

    Update

--- app/app.jsx
+++ app/app.jsx
@@ -1,13 +1,9 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
-var Main = require('Main');
 
 
 ReactDOM.render(
-  <Router history={hashHistory}>
-    <Route path="/" component={Main}>
-    </Route>
-  </Router>,
+  <p>React Boilerplate</p>,
   document.getElementById('app')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
   resolve: {
     root: __dirname,
     alias: {
-      Main: 'app/components/Main.jsx',
+      
     },
     extensions: ['', '.js', '.jsx']
   },
